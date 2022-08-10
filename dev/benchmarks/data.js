d624e732054bbcd47216da3b99770efb317929b4
github-action-benchmark <github@users.noreply.github.com>
2022-08-10 11:40:44 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for c22da114c6db6be7ee115b94d08337d22b5e8d0d

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660102546524,
+  "lastUpdate": 1660102844074,
   "repoUrl": "https://github.com/qzheng527/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -36,6 +36,56 @@ window.BENCHMARK_DATA = {
             "value": 3707,
             "unit": "Mbits/sec",
             "extra": "receiver"
+          }
+        ]
+      }
+    ],
+    "Sysbench Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "huaiqing.zq@antgroup.com",
+            "name": "Zheng, Qi",
+            "username": "qzheng527"
+          },
+          "committer": {
+            "email": "huaiqing.zq@antgroup.com",
+            "name": "Zheng, Qi",
+            "username": "qzheng527"
+          },
+          "distinct": true,
+          "id": "c22da114c6db6be7ee115b94d08337d22b5e8d0d",
+          "message": "[benchmark] Use dev/benchmark branch to publibc result",
+          "timestamp": "2022-08-10T11:29:56+08:00",
+          "tree_id": "87bb26450eff1ab5385479227fc59bda5358fab4",
+          "url": "https://github.com/qzheng527/ngo/commit/c22da114c6db6be7ee115b94d08337d22b5e8d0d"
+        },
+        "date": 1660102843062,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.05,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 52.08,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 753.92,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 277.21,
+            "unit": "ms",
+            "extra": "per95"
           }
         ]
       }
