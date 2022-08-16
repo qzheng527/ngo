efb890932871b13dc86a0c85a320aadac7457b6f
github-action-benchmark <github@users.noreply.github.com>
2022-08-16 09:49:13 +0800

    add Iperf3 Benchmark (customBiggerIsBetter) benchmark result for 49a55608604681e7fbf1d9ad49cce3623b9b1b1e

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1660296015282,
+  "lastUpdate": 1660614553659,
   "repoUrl": "https://github.com/qzheng527/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -214,6 +214,42 @@ window.BENCHMARK_DATA = {
           {
             "name": "Receiver Average Rate",
             "value": 3710,
+            "unit": "Mbits/sec",
+            "extra": "receiver"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "geding.lq@antgroup.com",
+            "name": "LI Qing",
+            "username": "liqinggd"
+          },
+          "committer": {
+            "email": "volcano.dr@hotmail.com",
+            "name": "volcano",
+            "username": "volcano0dr"
+          },
+          "distinct": true,
+          "id": "49a55608604681e7fbf1d9ad49cce3623b9b1b1e",
+          "message": "Fix the path length limit and update sefs",
+          "timestamp": "2022-08-15T09:51:09+08:00",
+          "tree_id": "f8f95c3863faf1a4fab6802ddbc05b0797da90ea",
+          "url": "https://github.com/qzheng527/ngo/commit/49a55608604681e7fbf1d9ad49cce3623b9b1b1e"
+        },
+        "date": 1660614552806,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sender Average Rate",
+            "value": 3730,
+            "unit": "Mbits/sec",
+            "extra": "sender"
+          },
+          {
+            "name": "Receiver Average Rate",
+            "value": 3730,
             "unit": "Mbits/sec",
             "extra": "receiver"
           }
