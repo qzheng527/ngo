ee4b9f8f3b2273cd77e3a2844974652050d3c6e9
github-action-benchmark <github@users.noreply.github.com>
2023-01-09 06:44:48 +0800

    add FIO Benchmark (customBiggerIsBetter) benchmark result for f0739c6d912e5c0ee859d939b5a6d6a16efcbec0

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673215933110,
+  "lastUpdate": 1673217888632,
   "repoUrl": "https://github.com/qzheng527/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -2452,6 +2452,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Random Read Throughput",
             "value": 211,
+            "unit": "MiB/s",
+            "extra": "randread"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Zheng, Qi",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "Zheng, Qi",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "id": "f0739c6d912e5c0ee859d939b5a6d6a16efcbec0",
+          "message": "[ci] Clean up repo in the end",
+          "timestamp": "2023-01-06T14:08:31Z",
+          "url": "https://github.com/qzheng527/ngo/commit/f0739c6d912e5c0ee859d939b5a6d6a16efcbec0"
+        },
+        "date": 1673217887674,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Sequential Write Throughput",
+            "value": 45.5,
+            "unit": "MiB/s",
+            "extra": "seqwrite"
+          },
+          {
+            "name": "Random Write Throughput",
+            "value": 43.7,
+            "unit": "MiB/s",
+            "extra": "randwrite"
+          },
+          {
+            "name": "Sequential Read Throughput",
+            "value": 324,
+            "unit": "MiB/s",
+            "extra": "seqread"
+          },
+          {
+            "name": "Random Read Throughput",
+            "value": 210,
             "unit": "MiB/s",
             "extra": "randread"
           }
