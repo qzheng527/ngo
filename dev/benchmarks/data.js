8cd9acef3631295414e683d8175f3e1f104e7905
github-action-benchmark <github@users.noreply.github.com>
2023-01-09 06:50:51 +0800

    add Sysbench Benchmark (customSmallerIsBetter) benchmark result for f0739c6d912e5c0ee859d939b5a6d6a16efcbec0

--- dev/benchmarks/data.js
+++ dev/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1673217888632,
+  "lastUpdate": 1673218251042,
   "repoUrl": "https://github.com/qzheng527/ngo",
   "entries": {
     "Iperf3 Benchmark": [
@@ -2036,6 +2036,52 @@ window.BENCHMARK_DATA = {
           {
             "name": "Thread 95th Percentile Latency",
             "value": 240.02,
+            "unit": "ms",
+            "extra": "per95"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Zheng, Qi",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "committer": {
+            "name": "Zheng, Qi",
+            "username": "qzheng527",
+            "email": "huaiqing.zq@antgroup.com"
+          },
+          "id": "f0739c6d912e5c0ee859d939b5a6d6a16efcbec0",
+          "message": "[ci] Clean up repo in the end",
+          "timestamp": "2023-01-06T14:08:31Z",
+          "url": "https://github.com/qzheng527/ngo/commit/f0739c6d912e5c0ee859d939b5a6d6a16efcbec0"
+        },
+        "date": 1673218250191,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Threads Minimum latency",
+            "value": 0.07,
+            "unit": "ms",
+            "extra": "min"
+          },
+          {
+            "name": "Threads Average Latency",
+            "value": 47.36,
+            "unit": "ms",
+            "extra": "avg"
+          },
+          {
+            "name": "Threads Maximum Latency",
+            "value": 622.65,
+            "unit": "ms",
+            "extra": "max"
+          },
+          {
+            "name": "Thread 95th Percentile Latency",
+            "value": 244.38,
             "unit": "ms",
             "extra": "per95"
           }
